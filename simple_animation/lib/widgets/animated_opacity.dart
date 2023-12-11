78044435fd6b69239edfc1ce3e303cfa09921136
JeoVn <132755008+JeoVn@users.noreply.github.com>
2023-12-11 14:04:51 +0700

    simple animated

---
+++ simple_animation/lib/widgets/animated_opacity.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class _AnimatedOpacityWidgetState extends StatelessWidget {
+  const _AnimatedOpacityWidgetState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
